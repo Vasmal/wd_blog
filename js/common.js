5ccb7e5640392c5998f3392e5b8f3cdf67c1dab3
unknown <malaivasili@gmail.com>
2019-04-07 18:18:29 +0300

    Сверстал блок с каруселью + Частично структура футера

--- js/common.js
+++ js/common.js
@@ -1,4 +1,26 @@
-$(document).ready(function(){
+$(document).ready(function () {
   var mixer = mixitup('.gallery');
+
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 2
+      },
+      480: {
+        items: 3
+      },
+      768: {
+        items: 5
+      },
+      1280: {
+        items: 8
+      },
+    }
+  });
 
 });
